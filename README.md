1bf02f06d70d2e6adbef814e6325d72f7de951b5
gabriel <gabriel.goncal.freitas@gmail.com>
2023-10-04 20:34:52 -0300

    criacao do app

---
+++ README.md
@@ -0,0 +1,3 @@
+# ponto_de_venda_flutter
+
+App feito para Web, Mobile e Desktop para pontos de venda.
---
+++ android/app/src/main/kotlin/com/example/ponto_de_venda_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ponto_de_venda_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
